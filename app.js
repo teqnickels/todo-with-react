88bbf8b4576217a035fba6d12eb75e46159a8d23
Debrena McEwen <debrenamcewen@gmail.com>
2017-04-15 12:44:13 -0700

    add setup for react app

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path')
+const app = express()
+
+
+
+app.use('/', (request, response) => {
+  response.send('HELLO YOU.')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen('3000')
+console.log("listening on port 3000");
+module.exports = app
