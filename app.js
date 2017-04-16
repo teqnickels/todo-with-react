04d16e60a8a01f522301fa2104331971c7adadf9
dratech <alfonsotech@nym.hush.com>
2017-04-15 18:04:19 -0700

    express and react hook-up

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
 const express = require('express')
 const path = require('path')
-const port = (process.env.PORT || 3000)
+const port = process.env.PORT || 3000
 const app = express()
-const router = require('./routes/router.js')
+var server = require('http').createServer(app)
 
-app.use(express.static(path.resolve(__dirname + '/public')))
-app.use('/', router)
-
-
-// const indexPath = path.join(__dirname, './src/client/index.html')
-// const publicPath = express.static(path.join(__dirname, './src/public'))
-// app.use('/public', publicPath)
-// app.get('/', (request, response) => {
-//   response.sendFile(indexPath)
-// })
-
-
-
-
-
-app.listen(port, function(){
-  console.log("listening on " +`${port}`)
-
+app.set('view engine', server)
+app.use(express.static('public'))
+app.get('/', function(request, response){
+  response.sendFile('./index.html');
 })
+
+
+server.listen(port)
+
 
 module.exports = app
